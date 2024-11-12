8212524269ce5175be3dc071a2a5735b8c9ef3a4
Keagan <keygun2k1@gmail.com>
2024-11-12 18:49:13 +0100

    Add lethals

---
+++ src/loadouts/lethals/index.ts
@@ -0,0 +1,41 @@
+import {Item} from "@/types";
+import {LethalNames} from "@/types/lethals";
+
+export const lethals: Item<LethalNames>[] = [
+    {
+        name: "Frag",
+        image: "/lethals/frag.png",
+    },
+    {
+        name: "Semtex",
+        image: "/lethals/semtex.png",
+    },
+    {
+        name: "Molotov",
+        image: "/lethals/molotov.png",
+    },
+    {
+        name: "C4",
+        image: "/lethals/c4.png",
+    },
+    {
+        name: "Thermo grenade",
+        image: "/lethals/thermo-grenade.png",
+    },
+    {
+        name: "Impact grenade",
+        image: "/lethals/impact-grenade.png",
+    },
+    {
+        name: "Blast trap",
+        image: "/lethals/blast-trap.png",
+    },
+    {
+        name: "Drill charge",
+        image: "/lethals/drill-charge.png",
+    },
+    {
+        name: "Combat Axe",
+        image: "/lethals/combat-axe.png",
+    }
+]
---
+++ src/types/lethals/index.ts
@@ -0,0 +1,9 @@
+export type LethalNames = "Frag"
+    | "Semtex"
+    | "Molotov"
+    | "C4"
+    | "Thermo grenade"
+    | "Impact grenade"
+    | "Blast trap"
+    | "Drill charge"
+    | "Combat Axe";
