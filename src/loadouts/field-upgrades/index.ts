7cd9f3d1ca26aa1fb8971bef68651fe782ef23b9
Keagan <keygun2k1@gmail.com>
2024-11-12 19:00:47 +0100

    Add field upgrades

---
+++ src/loadouts/field-upgrades/index.ts
@@ -0,0 +1,49 @@
+import {Item} from "@/types";
+import {FieldUpgradeNames} from "@/types/field-upgrades";
+
+export const fieldUpgrades: Item<FieldUpgradeNames>[] = [
+    {
+        name: "Assault pack",
+        image: "/attachments/field-upgrades/assault-pack.png",
+    },
+    {
+        name: "Spring mine",
+        image: "/attachments/field-upgrades/spring-mine.png",
+    },
+    {
+        name: "Throphy system",
+        image: "/attachments/field-upgrades/throphy-system.png",
+    },
+    {
+        name: "Scrambler",
+        image: "/attachments/field-upgrades/scrambler.png",
+    },
+    {
+        name: "Signal lure",
+        image: "/attachments/field-upgrades/signal-lure.png",
+    },
+    {
+        name: "War cry",
+        image: "/attachments/field-upgrades/war-cry.png",
+    },
+    {
+        name: "Tactical insertion",
+        image: "/attachments/field-upgrades/tactical-insertion.png",
+    },
+    {
+        name: "Acoustic amp",
+        image: "/attachments/field-upgrades/acoustic-amp.png",
+    },
+    {
+        name: "Morphine injector",
+        image: "/attachments/field-upgrades/morphine-injector.png",
+    },
+    {
+        name: "Neurogas",
+        image: "/attachments/field-upgrades/neurogas.png",
+    },
+    {
+        name: "Sleeper agent",
+        image: "/attachments/field-upgrades/sleeper-agent.png",
+    },
+];
