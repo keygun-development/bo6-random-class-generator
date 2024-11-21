26dcb1e732bca6b3f6b9c74fb2e91ed2350bfce4
Keagan <keygun2k1@gmail.com>
2024-11-21 23:55:49 +0100

    A little better FE

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,66 @@
-import "./App.css";
 import {xm4} from "@/loadouts/primary-guns/assault-rifles/xm4.ts";
+import {Checkbox} from "@/components/ui/checkbox.tsx";
+import Card from "@/components/Card.tsx";
+import {useState} from "react";
 
 function App() {
-  console.log(xm4.attachments.muzzles)
+    const [includeScorestreaks, setIncludeScorestreaks] = useState(true)
+    console.log(xm4.attachments.muzzles)
 
-  return (
-      <>
-
-      </>
-  );
+    return (
+        <div className="mt-4 mb-8 min-h-full md:px-10 mx-auto xl:px-20 2xl:max-w-[1280px] 2xl:px-0 w-full px-4">
+            <div className="flex justify-between items-center">
+                <div className="space-x-2">
+                    <Checkbox
+                        onCheckedChange={(e: boolean) => setIncludeScorestreaks(e)}
+                        checked={includeScorestreaks}/>
+                    <span className="text-white">
+                        Include scorestreaks?
+                    </span>
+                </div>
+            </div>
+            <div className="grid grid-cols-4 gap-4 mt-4">
+                <h1 className="col-span-3 text-white text-4xl">
+                    Loadout
+                </h1>
+                {includeScorestreaks && (
+                    <h2 className="col-span-1 text-white text-4xl">
+                        Scorestreaks
+                    </h2>
+                )}
+            </div>
+            <div className="grid grid-cols-4 gap-4 mt-4">
+                <div className="col-span-3">
+                    <Card/>
+                    <div className="grid grid-cols-2 gap-4 mt-4">
+                        <Card/>
+                        <Card/>
+                        <div className="grid grid-cols-2 gap-4">
+                            <Card/>
+                            <Card/>
+                        </div>
+                        <Card/>
+                        <div className="grid grid-cols-2 gap-4">
+                            <Card/>
+                            <Card/>
+                        </div>
+                        <div className="grid grid-cols-2 gap-4">
+                            <Card/>
+                            <Card/>
+                        </div>
+                    </div>
+                    <Card className="mt-4" />
+                </div>
+                {includeScorestreaks && (
+                    <div className="col-span-1 flex flex-col gap-4">
+                    <Card/>
+                        <Card/>
+                        <Card/>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
