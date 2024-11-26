0f094cc0eb86ea23dcab876f6307435c544b126c
Keagan <keygun2k1@gmail.com>
2024-11-26 11:33:29 +0100

    Open drawer when clicking card, re-randomize button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import {Checkbox} from "@/components/ui/checkbox.tsx";
-import Card from "@/components/Card.tsx";
+import {Card} from "@/components/Card.tsx";
 import {useState} from "react";
 import {Randomizer} from "@/lib/Randomize.ts";
 import {scorestreaks} from "@/scorestreaks";
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer"
+import {Button} from "@/components/ui/button.tsx";
 
 function App() {
     const [includeScorestreaks, setIncludeScorestreaks] = useState(true)
     const randomizer = new Randomizer(scorestreaks);
-    const randomScorestreaks = randomizer.randomize(3);
+    const [randomScorestreaks, setRandomScorestreaks] = useState(randomizer.randomize(3));
     randomizer.reset();
 
+    const reRoll = () => {
+        setRandomScorestreaks(randomizer.randomize(3));
+        randomizer.reset()
+    }
     return (
         <div className="mt-4 mb-8 min-h-full md:px-10 mx-auto xl:px-20 2xl:max-w-[1280px] 2xl:px-0 w-full px-4">
             <div className="flex justify-between items-center">
@@ -22,6 +37,9 @@ function App() {
                         Include scorestreaks?
                     </span>
                 </div>
+                <Button onClick={() => reRoll()} variant="primary">
+                    Randomize
+                </Button>
             </div>
             <div className="md:grid grid-cols-4 gap-4 mt-4">
                 <h1 className="col-span-3 text-white lg:text-4xl text-2xl md:text-2xl">
@@ -35,7 +53,24 @@ function App() {
             </div>
             <div className="md:grid grid-cols-4 gap-4 mt-4">
                 <div className="col-span-3">
-                    <Card/>
+                    <Drawer>
+                        <DrawerTrigger asChild>
+                            <Card/>
+                        </DrawerTrigger>
+                        <DrawerContent>
+                            <DrawerHeader>
+                                <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                                <DrawerDescription>This action cannot be undone.</DrawerDescription>
+                            </DrawerHeader>
+                            <DrawerFooter>
+                                <Button>Submit</Button>
+                                <DrawerClose>
+                                    <Button variant="outline">Cancel</Button>
+                                </DrawerClose>
+                            </DrawerFooter>
+                        </DrawerContent>
+                    </Drawer>
+
                     <div className="grid grid-cols-2 gap-4 mt-4">
                         <Card/>
                         <Card/>
